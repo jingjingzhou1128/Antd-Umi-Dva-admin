5f3efb61b26eb10949c175da4e7253ddb07c2f6c
jingjingzhou1128 <ZJJ_zjj1128@163.com>
2019-12-31 18:01:15 +0800

    init project

---
+++ .umirc.js
@@ -0,0 +1,40 @@
+
+// ref: https://umijs.org/config/
+export default {
+  base: '/',
+  history: 'hash',
+  outputPath: './dist',
+  treeShaking: true,
+  routes: [
+    {
+      path: '/',
+      component: '../layouts/index',
+      routes: [
+        { path: '/', component: '../pages/index' }
+      ]
+    }
+  ],
+  plugins: [
+    // ref: https://umijs.org/plugin/umi-plugin-react.html
+    ['umi-plugin-react', {
+      antd: true,
+      dva: true,
+      dynamicImport: { webpackChunkName: true },
+      title: 'React-test',
+      dll: false,
+      locale: {
+        enable: true,
+        default: 'en-US',
+      },
+      routes: {
+        exclude: [
+          /models\//,
+          /services\//,
+          /model\.(t|j)sx?$/,
+          /service\.(t|j)sx?$/,
+          /components\//,
+        ],
+      },
+    }],
+  ],
+}
